5f27358b037b0776ce6637de949f216093396d49
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-05-04 22:12:05 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -17,10 +17,6 @@ var appCertificate = process.env.APP_CERTIFICATE;
 
 const expirationTimeInSeconds = 3600; // 1 hour
 
-const currentTimestamp = Math.floor(Date.now() / 1000);
-
-const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-
 var role = RtcRole.PUBLISHER;
 
 var app = express();
@@ -29,58 +25,6 @@ app.set("port", PORT);
 app.use(express.favicon());
 app.use(app.router);
 
-function nocache(req, res, next) {
-  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
-  res.header("Expires", "-1");
-  res.header("Pragma", "no-cache");
-  next();
-}
-
-var generateAccessToken = function (req, resp) {
-  resp.header("Access-Control-Allow-Origin", "*");
-
-  var channel = req.query.channel;
-  if (!channel) {
-    return resp.status(500).json({ error: "channel name is required" });
-  }
-
-  var uid = req.query.uid;
-  if (!uid) {
-    uid = 0;
-  }
-
-  var expiredTs = req.query.expiredTs;
-  if (!expiredTs) {
-    expiredTs = privilegeExpiredTs;
-  }
-
-  var role = req.query.role;
-  if (!role) {
-    role = RtcRole;
-  }
-
-  const tokenA = RtcTokenBuilder.buildTokenWithUid(
-    APP_ID,
-    APP_CERTIFICATE,
-    channel,
-    uid,
-    role,
-    expiredTs
-  );
-  return resp.json({ token: tokenA });
-};
-
-app.get("/access_token", nocache, generateAccessToken);
-
-app.listen(PORT, function () {
-  console.log("Service URL http://127.0.0.1:" + PORT + "/");
-  console.log(
-    "Channel Key request, /access_token?uid=[user id]&channel=[channel name]&role=[RtcRole.PUBLISHER]"
-  );
-  console.log(
-    "Channel Key with expiring time request, /access_token?uid=[user id]&channel=[channel name]&expiredTs=[expire ts]"
-  );
-});
 var generateRtcToken = function (req, resp) {
   var currentTimestamp = Math.floor(Date.now() / 1000);
   var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
